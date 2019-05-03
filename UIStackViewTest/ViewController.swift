0d3619b6ec5a855bd70b4b9cd5e8ab769194c4b2
Sayaka Saito <s.saito0829@gmail.com>
2019-05-03 21:12:39 +0900

    Initial Commit

---
+++ UIStackViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIStackViewTest
+//
+//  Created by sayaka on 2019/05/03.
+//  Copyright © 2019年 Sayaka Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
